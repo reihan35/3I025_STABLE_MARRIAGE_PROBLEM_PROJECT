7f8e20ae216d1492cb0129d53e09f03330ffbf0f
laudao <nguyen.laura97@gmail.com>
2018-02-07 00:00:40 +0100

    created plot for gs results

--- gs/testGS.py
+++ gs/testGS.py
@@ -1,13 +1,28 @@
 from gs_tab import *
 from generePref import *
 import timeit
+#import os
+import matplotlib
+import matplotlib.pyplot as plt
 
-for n in range(200, 2000):
+#if os.path.isfile("tempscalculs.txt"):
+#	os.remove("tempscalculs.txt")
+
+x = []
+y = []
+
+for n in range(100, 1000):
 	prefEtu = generePrefEtu(n)
 	prefSpe = generePrefParcours(n)
 	start = timeit.default_timer()
-	file = open("tempscalculs.txt","a")
+#	file = open("tempscalculs.txt","a")
 	mariage = gs_etudiants(prefEtu, n, prefSpe, 9)
 	stop = timeit.default_timer()
-	file.write(str(n)+" "+str(start-stop)+"\n")
-	file.close()
+	
+	x.append(n)
+	y.append(stop-start)
+#	file.write(str(n)+" "+str(stop-start)+"\n")
+#	file.close()
+
+plt.plot(x, y)
+plt.show()
