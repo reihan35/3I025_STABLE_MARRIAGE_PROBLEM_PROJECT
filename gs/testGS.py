4bcd8b342f63f36b5930e282e2d1df59d56f3695
Fati <reihaneh77@yahoo.com>
2018-02-06 20:08:27 +0100

    results for 200-2000

--- gs/testGS.py
+++ gs/testGS.py
@@ -2,12 +2,12 @@ from gs_tab import *
 from generePref import *
 import timeit
 
-for n in range(10, 100):
+for n in range(200, 2000):
 	prefEtu = generePrefEtu(n)
 	prefSpe = generePrefParcours(n)
 	start = timeit.default_timer()
 	file = open("tempscalculs.txt","a")
 	mariage = gs_etudiants(prefEtu, n, prefSpe, 9)
 	stop = timeit.default_timer()
-	file.write("%d %d", n, stop - start )
+	file.write(str(n)+" "+str(start-stop)+"\n")
 	file.close()
